d504c4755458b630da02382aeca7cf34f5b78e7e
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-05 11:45:37 +0000

    ConditionExamples

---
+++ ConditionExample.js
@@ -0,0 +1,25 @@
+//Odd?Even?
+n=15;
+if(n%2==1){
+	console.log("odd number");
+	
+}
+else{
+	console.log("Even number");
+}
+
+//GreaterNum?
+
+let x=10;
+let y=10;
+if(x>y){
+	console.log("x>y");
+}
+else if(x==y){
+	
+	console.log("y=x");
+}
+else{
+	console.log("y>x");
+	
+}
