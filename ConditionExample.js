e1f56bf5d6019075b5d06652a758700a1124a55b
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-05 12:17:32 +0000

    SwitchCases

--- ConditionExample.js
+++ ConditionExample.js
@@ -1,25 +1,41 @@
 //Odd?Even?
-n=15;
-if(n%2==1){
-	console.log("odd number");
-	
+n = 15;
+if (n % 2 == 1) {
+    console.log("odd number");
+
 }
-else{
-	console.log("Even number");
+else {
+    console.log("Even number");
 }
 
 //GreaterNum?
 
-let x=10;
-let y=10;
-if(x>y){
-	console.log("x>y");
+let x = 10;
+let y = 10;
+if (x > y) {
+    console.log("x>y");
 }
-else if(x==y){
-	
-	console.log("y=x");
+else if (x == y) {
+
+    console.log("y=x");
 }
-else{
-	console.log("y>x");
-	
+else {
+    console.log("y>x");
+
+}
+
+
+
+//Switchcase
+statecode = "ts"
+switch (statecode) {
+    case "ts":
+        console.log("Hyd");
+        break;
+    case "tn":
+        console.log("Chn");
+        break;
+    default:
+        console.log("No state found");
+        break;
 }
