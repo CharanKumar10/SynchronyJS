2b7c892cee134443d02d0798fecda9f4ff71395d
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-05 11:08:25 +0000

    Demo code

--- Demo.js
+++ Demo.js
@@ -1,1 +1,15 @@
+"use strict"//Forces user to declare variable
 
+console.log("Hello");
+let a=2;
+let b=a;
+let c=b;
+console.log(c);
+let c1=19;
+console.log(c1);
+let name="Charan";
+console.log(name);
+let array=["aa",1,2,3];
+console.log(array);
+array.push(221.5,123);
+console.log(array);
