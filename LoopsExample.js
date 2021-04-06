a620cc69a13b71114bd01868dafc219ab46ee53b
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-06 09:20:09 +0000

    ForLoop

---
+++ LoopsExample.js
@@ -0,0 +1,44 @@
+"use strict"
+//for loop
+//numbers from 1-10
+console.log("numbers from 1-10");
+for(let i=1;i<=10;i++){
+	
+	console.log(i);
+	}
+	
+//Even numbers btwn 20-40 +2
+console.log("Even numbers btwn 20-40 +2");
+for(let i=20;i<=40;i+=2){
+	
+	console.log(i);
+	}
+	
+//Odd numbers btwn 41-51 +2
+console.log("Odd numbers btwn 41-51 +2");
+for(let i=41;i<=51;i+=2){
+	
+	console.log(i);
+	}
+	
+//numbers from 10-1
+console.log("numbers from 10-1 -1");
+for(let i=10;i>=1;i--){
+	
+	console.log(i);
+    }	
+
+//print 10 for 10 times
+for(i=0;i<10;i++){
+
+    console.log(i);
+
+}
+
+
+//while
+j=10;
+while(j>0){
+console.log(j);
+j--;
+}
