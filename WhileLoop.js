0d46029a970da9992232e39bdffe03ca5df59ef7
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-06 11:12:51 +0000

    WhileLoop

---
+++ WhileLoop.js
@@ -0,0 +1,16 @@
+"use strict"
+console.log("While");
+let n=1;
+
+while(n<10){
+
+    console.log(n);
+    n++;    
+
+}
+console.log("Do..While");
+
+do{
+    n++;
+    console.log(n);
+}while(n<10)
