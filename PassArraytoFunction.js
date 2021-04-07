6f1e3ab0ddfdf11d197562182f13645b5c160dff
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-07 10:09:53 +0000

    ArrayInFunction

---
+++ PassArraytoFunction.js
@@ -0,0 +1,17 @@
+"use strict"
+function add(a,b){
+
+    let sum=a+b;
+    return sum;
+}
+console.log(add(2,5));
+
+function addAll(n){
+    let sum=0;
+    for(let i=0;i<n.length;i++){
+        sum=sum+n[i];
+    }
+    return sum;
+}
+let arr=[10,20,30];
+console.log(addAll(arr));
