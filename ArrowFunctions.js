a218809f13c025ce350a70cbff91a806914a8293
CharanKumar10 <charan.rgukt09@gmail.com>
2021-04-08 08:41:50 +0000

    arrowfunc

--- ArrowFunctions.js
+++ ArrowFunctions.js
@@ -2,11 +2,10 @@
 
 let data =[10,20,30];
 //short form for anonymous function
-data.forEach(element =>console.log(element););
+data.forEach(element =>console.log(element));
 
-//syntax for short form
-//()=>{}
-//() is option if called function has single parameter
+//syntax for short form is ()=>{}
+//() is optional if called function has single parameter
 //{} is optional if called function has single line
 // this is called lambda expression
 //used to simplify creating of the function containing single line
@@ -19,19 +18,17 @@ calculator(2,3,function(a,b){
     return a+b
 });
 
-calculator(2,3,(a,b)=>{return a+b});
-calculator(2,3,(a,b)=>a+b);//single line so {} is optional & {} is not used single line becomes return value
+console.log(calculator(2,3,(a,b)=>{return a+b}));
+
+console.log(calculator(2,4,(a,b)=>a+b)); //single line so {} is optional & {} is not used single line becomes return value
 //fat arrow function
-
-
-
 
 
 function add(x,y,add){
     return add(x,y);
 }
 
-add(20,40,function(a,b){return a+b;});
+console.log(add(20,30,function(a,b){return a+b;})); 
 
-add(20,40,(a,b)=>{a+b});
-add(20,40,(a,b)=>a+b;);
+console.log(add(20,40,(a,b)=>{return a+b})); 
+console.log(add(20,50,(a,b)=>a+b)); //called function has single line
